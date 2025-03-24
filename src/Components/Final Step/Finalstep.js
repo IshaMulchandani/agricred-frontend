a658b49a13d8c0d8eb3bedf646384c47bdb74e1b
Isha Mulchandani <ishamulani1@gmail.com>
2025-03-24 20:00:12 +0530

    Third Commit

--- src/Components/Final Step/Finalstep.js
+++ src/Components/Final Step/Finalstep.js
@@ -173,7 +173,7 @@ export default function Finalstep(){
                    <Link to="/property"> Back </Link>
                 </button>
                 <button type="submit" class="px-6 py-2 bg-green-600 text-white rounded-lg text-sm font-medium hover:bg-green-700">
-                   <a href="#"> Submit Application </a>
+                   <Link to="/final-step"> Submit Application </Link>
                 </button>
             </div>
         </div>
