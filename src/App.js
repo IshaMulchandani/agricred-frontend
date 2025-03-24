d910a0ec3e87d7fd0467ac021e3a52d8651de800
Isha Mulchandani <ishamulani1@gmail.com>
2025-03-24 17:35:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Registration from './Components/Registration/Registration';
+import Farmdetials from './Components/Farmdetails/Farmdetails';
+import Generalinformation from './Components/General Information/Generalinformation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Registration/>}/>
+        <Route path='/farm-details' element={<Farmdetials/>}/>
+        <Route path='/general-information' element={<Generalinformation/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/General Information/Generalinformation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Generalinformation.css'
+import { Link } from 'react-router-dom'
+
+export default function Generalinformation(){
+    <div className="genInfoCont">
+        
+    </div>
+}
